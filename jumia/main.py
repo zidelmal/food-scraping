6ab6dbff971c37e73287aca97aef65c4e4628008
zidelmal <a.zidelmal96@gmail.com>
2023-07-09 14:51:22 +0100

    finished for deployement

--- jumia/main.py
+++ jumia/main.py
@@ -2,23 +2,27 @@ import pandas as pd
 from scrapers.jumiafood_tn import JumiaFoodTn
 from scrapers.jumiafood_ma import JumiaFoodMa
 
+SCRAPERS = [
+            (JumiaFoodMa(), "JumiaFoodMA"), 
+            (JumiaFoodTn(), "JumiaFoodTN")
+            ]
 
-SCRAPERS = [(JumiaFoodTn(), "JumiaFoodTN"), (JumiaFoodMa(), "JumiaFoodMA")]
+def main():
+    for scraper in SCRAPERS:
+        with scraper[0] as bot:
+            bot.land_first_page()
+            bot.first_popup()
+            informations = bot.get_restaurants_url()
+            data = pd.DataFrame()
+            for i, row in enumerate(informations):
+                try:
+                    bot.close_popup()
+                    item = bot.get_restaurant(row, data)
+                    data = pd.concat([data, pd.DataFrame(item)], ignore_index=True)
+                    data.to_csv(f'extracted_data/{scraper[1]}_processing.csv', index=False)
+                except:
+                    pass
+        data.to_csv(f'extracted_data/{scraper[1]}.csv', index=False)
 
-
-for scraper in SCRAPERS:
-    with scraper[0] as bot:
-        bot.land_first_page()
-        bot.first_popup()
-        informations = bot.get_restaurants_url()
-        print(informations)
-        data = pd.DataFrame()
-        for i, row in enumerate(informations):
-            try:
-                bot.close_popup()
-                item = bot.get_restaurant(row, data)
-                data = pd.concat([data, pd.DataFrame(item)], ignore_index=True)
-                data.to_csv(f'extracted_data/{scraper[1]}.csv', index=False)
-            except Exception as e:
-                print(e)
-                pass
+if __name__ == '__main__':
+    main()
